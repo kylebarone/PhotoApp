5a6cb4d212cea25f31ba84d4bad01748a1768f49
kbarone6 <kbarone@gatech.edu>
2020-07-20 21:39:46 -0700

    Configured Zuul api gateway to restrict non authorized acess

---
+++ ZuulAPIGateway/src/main/java/com/appsdeveloperblog/photoapp/api/ZuulAPIGateway/ZuulAPIGateway/Security/AuthorizationFilter.java
@@ -0,0 +1,59 @@
+package com.appsdeveloperblog.photoapp.api.ZuulAPIGateway.ZuulAPIGateway.Security;
+
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    Environment environment;
+
+    public AuthorizationFilter(AuthenticationManager authenticationManager, Environment environment) {
+        super(authenticationManager);
+        this.environment = environment;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
+        String authorizationToken = request.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationToken == null || !authorizationToken.startsWith(environment.getProperty("authorization.token.header.prefix"))) {
+            chain.doFilter(request, response);
+            return;
+        }
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationToken == null) {
+            return null;
+        }
+
+        String token = authorizationHeader.replace(environment.getProperty("authorization.token.header.prefix"), "");
+
+        String userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("token.secret"))
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        if (userId == null) {
+            return null;
+        }
+        return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+    }
+}
---
+++ ZuulAPIGateway/src/main/java/com/appsdeveloperblog/photoapp/api/ZuulAPIGateway/ZuulAPIGateway/Security/WebSecurity.java
@@ -0,0 +1,40 @@
+package com.appsdeveloperblog.photoapp.api.ZuulAPIGateway.ZuulAPIGateway.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity  extends WebSecurityConfigurerAdapter {
+
+    private final Environment environment;
+
+    @Autowired
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        //this restricts the endpoint acess
+        //this is enabling endpoints that dont require authorization header
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.users-ws.login.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.users-ws.registration.path")).permitAll()
+                .antMatchers(HttpMethod.GET, environment.getProperty("api.users-ws.status.path")).permitAll()
+                .antMatchers(environment.getProperty("api.users-ws.console.path")).permitAll()
+                .anyRequest().authenticated();
+
+        //telling spring not to create a http session that caches cookies that might mess up how our shit is authorized
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
