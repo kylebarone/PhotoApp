901071e33ff73a8ffdcb4242e9e8eadfb0ce9253
kbarone6 <kbarone@gatech.edu>
2020-08-23 14:25:22 -0400

    Added Base Impl for Albums Microservice

---
+++ PhotoAppAlbums/src/test/java/com/appsdeveloperblog/photoapp/api/PhotoAppAlbums/PhotoAppAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.PhotoAppAlbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAlbumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
