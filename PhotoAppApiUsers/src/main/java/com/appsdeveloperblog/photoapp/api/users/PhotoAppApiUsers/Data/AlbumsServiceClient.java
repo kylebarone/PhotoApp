6b2490c6881116db74ec46feddb5b842d6225b23
kbarone6 <kbarone@gatech.edu>
2020-08-27 13:53:43 -0400

    Added error handling and circuit breaker compatability

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsers/Data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsers/Data/AlbumsServiceClient.java
@@ -1,10 +1,16 @@
 package com.appsdeveloperblog.photoapp.api.users.PhotoAppApiUsers.Data;
 
 import com.appsdeveloperblog.photoapp.api.users.PhotoAppApiUsers.Model.AlbumResponseModel;
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,9 +18,39 @@ import java.util.List;
  * declare it and the method that we wanna be able to call
  */
 
-@FeignClient(name = "albums-ws") //this is the name of the microservice in which feign is communicating with
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallBackFactory.class) //this is the name of the microservice in which feign is communicating with
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
     public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsFallBackFactory implements FallbackFactory<AlbumsServiceClient> {
+
+    @Override
+    public AlbumsServiceClient create(Throwable throwable) {
+        return new AlbumsServiceClientFallback(throwable);
+    }
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+
+    private final Throwable cause;
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public AlbumsServiceClientFallback(Throwable cause) {
+        this.cause = cause;
+    }
+
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+            logger.error("404 error took place when getAlbums was called with userId: "
+            + id + ". Error Message: " + cause.getLocalizedMessage());
+        } else {
+            logger.error("Other error took place: " + cause.getLocalizedMessage());
+        }
+        return new ArrayList<>();
+    }
 }
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -2,12 +2,14 @@ package com.appsdeveloperblog.photoapp.api.users.PhotoAppApiUsers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient // this makes this app a client to discovery and communicates
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
